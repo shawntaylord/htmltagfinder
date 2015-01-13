229d5f0ac6134aa3b2bf3eb2e362b3b4335d1ac6
Shawn Taylor <shawn.b.taylor23@gmail.com>
2015-01-12 23:08:07 -0800

    First draft of script

---
+++ htmltagger.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import re
+import sys
+
+def nameOfMissingTag(path_to_html_file):
+    open_regex = '\<(?P<opentag>[a-zA-Z]+)[^\/]*\>'
+    close_regex = '\<\/(?P<closetag>[a-zA-Z]*)\>'
+
+    bracket_dict = {}
+
+    file = open(path_to_html_file, 'r')
+
+    for line in file:
+        open_tags = re.findall(open_regex, line)
+        closed_tags = re.findall(close_regex, line)
+        if open_tags:
+            for tag in open_tags:
+                if tag in bracket_dict:
+                    bracket_dict[tag] += 1
+                else:
+                    bracket_dict[tag] = 1
+        if closed_tags:
+            for tag in closed_tags:
+                if tag in bracket_dict:
+                    bracket_dict[tag] -= 1
+                else:
+                    bracket_dict[tag] = -1
+
+    print bracket_dict
+
+if __name__ == "__main__":
+    nameOfMissingTag(sys.argv[1])
